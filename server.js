fe8a4d009a07393677cae6718be269d729bcfb49
adeshnalawade2010 <adeshnalawade2010@gmail.com>
2024-12-17 17:47:40 +0530

    node jenkins demo

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express=require('express')
 const app=express()
 app.get('/',(req,res)=>{
-    res.send('Hello Aarya! how are you ')
+    res.send('Hello Aarya! how are you!!!!! ')
 })
 app.listen(4000,'0.0.0.0',()=>{
     console.log('aarya  connected server')
